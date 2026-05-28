3a7f95a78e88dba546a9dc82856cdbcbd25ac328
Hasan Anıl Kartal <hasananilkartal@192.168.1.103>
2026-05-28 12:51:17 +0300

    Update projeler

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,4 @@
 
 export default new Map([
-["src/content/blog/ai-qa-engineer-degisiyor.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-qa-engineer-degisiyor.mdx&astroContentModuleFlag=true")],
-["src/content/projects/api-test-framework.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fapi-test-framework.mdx&astroContentModuleFlag=true")],
-["src/content/projects/e2e-test-altyapisi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fe2e-test-altyapisi.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/ai-qa-engineer-degisiyor.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fai-qa-engineer-degisiyor.mdx&astroContentModuleFlag=true")]]);
 		
